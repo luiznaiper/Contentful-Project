1ccca3cafef8ad29952c7c5819b2ba096df2fac5
luiznaiper <luiz_naiper@hotmail.com>
2023-07-05 09:06:33 -0600

    :construction: Add types

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,6 +1,9 @@
-import './fetchProjects'
+import { useFetchProjects } from './fetchProjects'
 
 const Project = () => {
+  const { loading, projects } = useFetchProjects()
+  console.log(loading, projects)
+
   return <h2>Project</h2>
 }
 
--- src/fetchProjects.tsx
+++ src/fetchProjects.tsx
@@ -1,4 +1,5 @@
-import { createClient } from 'contentful'
+import { createClient, Entry } from 'contentful'
+import { useEffect, useState } from 'react'
 
 const client = createClient({
   space: import.meta.env.VITE_APP_API_SPACE,
@@ -6,8 +7,50 @@ const client = createClient({
   accessToken: import.meta.env.VITE_APP_API_KEY,
 })
 
-client
-  .getEntries({
-    content_type: 'projects',
-  })
-  .then((response) => console.log(response))
+type Project = {
+  title: string
+  url: string
+  id: string
+  img: string | undefined
+}
+
+interface Fields {
+  title: string
+  url: string
+  image: {
+    fields: {
+      file: {
+        url: string
+      }
+    }
+  }
+}
+
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState<Project[]>([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({
+        content_type: 'projects',
+      })
+      const projects = response.items.map((item: Entry<unknown>) => {
+        const { title, url, image } = item.fields as Fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
+        return { title, url, id, img }
+      })
+      setProjects(projects)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  return { loading, projects }
+}
